548d5b2d7c68f5124c4460f27230eb7ce8a28e49
Dương Văn Hiệp <129377698+imdhiep@users.noreply.github.com>
2024-02-29 09:40:25 +0700

    Delete Q2_refer directory

--- Q2_refer/Given/Car.py
+++
@@ -1,6 +0,0 @@
-class Car:
-    def __init__(self, name="", price=-1):
-        self.Name = name
-        self.Price = price
-    def __repr__(self):
-        return f"({self.Name},{self.Price})"    
